2e9822f7c20bf682f2548001b583e6d00eb3bf5f
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2017-03-20 19:02:24 -0300

    whooops

---
+++ crawler/app.js
@@ -0,0 +1,12 @@
+import scraper from './src/scraper';
+import exporter from './src/exporter';
+
+scraper.fetchLinks()
+  .then((carList) => {
+    console.log(`found ${carList.length}`);
+    return exporter.saveCarsToJSON(carList);
+  }).then(() => {
+    console.log('saved =)');
+  }).catch((err) => {
+    console.log(err);
+  });
